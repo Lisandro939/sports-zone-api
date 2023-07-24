e59ece58038b3a13faf0bafedc1088fde2ffbbdd
Lisandro939 <lisandrofernandez2705@gmail.com>
2023-07-24 13:46:37 -0300

    Functions

--- src/functions/fetchFutbolTournaments.js
+++ src/functions/fetchFutbolTournaments.js
@@ -2,21 +2,21 @@ import axios from 'axios';
 import * as cheerio from 'cheerio'
 
 // https://www.espn.com.ar/futbol/torneos
-const url = 'https://www.espn.com.ar/futbol/posiciones/_/liga/bra.1'
+const url = 'https://www.espn.com.ar/futbol/torneos'
 
 async function getHTML () {
     const { data: html} = await axios.get(url)
     return html;
 };
 
-export function fetching () {
+export function fetchFutbolTournaments () {
     getHTML().then(async (res) => {
         const leagueNameClubs = [];
         const $ = cheerio.load(res);
-        const $tableNames = $('tbody>tr>td>div>span.hide-mobile>a').each((i, line) => {
+        const $tableNames = $('div>div>div>section>div>a>h2').each((i, line) => {
         const linea = $(line).text()
         leagueNameClubs.push(linea)
         })
-        console.log(leagueNameClubs)
+        console.log(leagueNameClubs.slice(0, 15))
     })
 }
