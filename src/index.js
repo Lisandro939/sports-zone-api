1ffa0f300c9d7a31e3fd7f7bd5c519a6f03f088b
Lisandro939 <lisandrofernandez2705@gmail.com>
2023-07-24 13:46:30 -0300

    Index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import express from 'express';
 
+import { config } from 'dotenv';
+config();
+
 // cron
-import cron from 'node-cron';
+// import cron from 'node-cron';
 
 // swagger
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUI from 'swagger-ui-express';
-import { fetching } from './functions/fetch.js';
+
+// SQL
+import mysql from 'mysql2';
+import { usersRouter } from './routes/userRoute.js';
+import { countryRouter } from './routes/countryRoute.js';
+import { sportRouter } from './routes/sportRoute.js';
+import { leagueRouter } from './routes/leagueRoute.js';
+import { teamRouter } from './routes/teamRoute.js';
+import { playerRouter } from './routes/playerRoute.js';
+
+export const connection = mysql.createConnection({
+    host: process.env.HOST_URL,
+    user: process.env.USER_DB,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
+
+connection.connect((error) => {
+    if (error) {
+      console.error('Error de conexión: ', error);
+    } else {
+      console.log('Conexión exitosa a la base de datos de Sports Zone');
+    }
+});
+
 
 const swaggerSpec = {
     definition: {
@@ -48,8 +75,14 @@ app.get('/', (req, res) => {
 }
 );
 
+app.use('/api/v1', usersRouter);
+app.use('/api/v1', countryRouter);
+app.use('/api/v1', sportRouter);
+app.use('/api/v1', leagueRouter);
+app.use('/api/v1', teamRouter);
+app.use('/api/v1', playerRouter);
+
 // Make a cron every 5 seconds
-/* cron.schedule('*/ /*15 * * * * *', () => {
-    fetching();
-}
-); */
+// cron.schedule('*/15 * * * * *', () => { ... });
+
+
